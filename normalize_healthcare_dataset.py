294923a6b7181c45046d94b8be1140aed6f9a187
Sawar Saini <sawar26saini@gmail.com>
2025-07-03 13:15:17 -0700

    normalizing age column first

--- normalize_healthcare_dataset.py
+++ normalize_healthcare_dataset.py
@@ -3,9 +3,22 @@ import pandas as pd
 import datetime
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
+# debugging
+print("Starting script...")
+
 #load dataset
 filename = r'C:\Users\sawar\OneDrive\UN_LearningPlanet\dataset_analysis\THE_datasets_not_cleaned\healthcare_dataset.csv'
 df = pd.read_csv(filename)
 
+#debugging 
+print("File loaded successfully.")
+
 # check the available columns
 print(df.columns)
+
+# normalize Age using Min-Max Scaling
+if 'Age' in df.columns:
+    scaler_age = MinMaxScaler()
+    df['Age_normalized'] = scaler_age.fit_transform(df[['Age']])
+    print("Age column is normalized using Min-Max Scaling.")
+
