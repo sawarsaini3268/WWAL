305dbc48a4eb22fe8fe3a52641765d5941e821af
Sawar Saini <sawar26saini@gmail.com>
2025-07-07 15:30:57 -0700

    creating json file for lists

--- extract_columns_outputFile.py
+++ extract_columns_outputFile.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 # load file 
 file_path = r"C:\Users\sawar\OneDrive\UN_LearningPlanet\dataset_analysis\not_data\Full_List_of_Columns_for_Categorization.csv.xlsx"
@@ -23,5 +24,22 @@ minmax_columns = df[df['Category_clean'] == 'Numerical (Min-Max scaling)']['Colu
 # numerical (Z-score)
 zscore_columns = df[df['Category_clean'] == 'Numerical (Z-score scaling)']['Column Name'].tolist()
 
+#checking lists
+print("Drop columns:", drop_columns)
+print("Date columns:", date_columns)
+print("Categorical columns:", categorical_columns)
+print("Min-Max columns:", minmax_columns)
+print("Z-score columns:", zscore_columns)
 
+# create a dictionary with categorized lists
+all_lists = {
+    "drop": drop_columns,
+    "date": date_columns,
+    "categorical": categorical_columns,
+    "minmax": minmax_columns,
+    "zscore": zscore_columns
+}
 
+# write dictionary to a file
+with open("column_groups.json", "w") as f:
+    json.dump(all_lists, f, indent=2)
