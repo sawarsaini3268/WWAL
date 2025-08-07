35f0cbc784637b8196c04c916cbbc9a25e67d586
Sawar Saini <sawar26saini@gmail.com>
2025-08-06 19:14:53 -0700

    realized path is wrong

--- master_dataset_2.py
+++ master_dataset_2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the output dataset
-output = pd.read_csv("THE_datasets_cleaned/output_file_cleaned_normalized_trimmed.csv")
+output = pd.read_csv(r"C:\Users\sawar\OneDrive\UN_LearningPlanet\dataset_analysis\THE_datasets_cleaned\trimmed\output_file_cleaned_normalized_trimmed.csv")
 
 # Show the first 50 column names
 print(output.columns[:50])
